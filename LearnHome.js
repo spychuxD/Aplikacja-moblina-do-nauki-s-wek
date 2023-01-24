ac1f9f78e9c0cc5b7d9779a08fbcf3f728dd3b84
spychuxD <dawid.spychalski00@gmail.com>
2023-01-24 17:31:25 +0100

    dopasowania

--- LearnHome.js
+++ LearnHome.js
@@ -46,7 +46,7 @@ const LearnHome = () => {
       </HStack>
     </Button>
     <Button style={{ shadowColor: 'black', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 }}
-    backgroundColor="#8aa29e" onPress={()=>navigation.navigate('DopasowaniaStart', {nazwa: itemid, size: size})}>
+    backgroundColor="#8aa29e" onPress={()=>navigation.navigate('Dopasowania', {nazwa: itemid, size: size})}>
       <HStack>
         <Icon as={<MaterialIcons name="extension" />} m="1" size={16} color="#f1edee" />
         <Text w={{base: '61%'}} m="3" fontSize="26px" color="#f1edee" bold>
